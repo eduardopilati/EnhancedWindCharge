0bd1ef61ce88f5b20f148fea6e38ea42d92679a1
Eduardo Pilati <pilatieduardo@gmail.com>
2024-07-23 20:16:50 -0300

    Development of Plugin

---
+++ src/main/java/dev/pilati/enhancedwindcharge/EnhancedWindCharge.java
@@ -0,0 +1,17 @@
+package dev.pilati.enhancedwindcharge;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EnhancedWindCharge extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
